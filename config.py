31e76e8ef26f6120dd27dfd78b996767f33959f2
Fabrice Verhaert <fabriceverhaert@gmail.com>
2021-12-15 17:54:35 +0100

    chore: testing the algorithm

--- config.py
+++ config.py
@@ -33,28 +33,28 @@ CONFIG = [
         'name': 'body',
         'directory': 'Body',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': 'random'
     },
     {
         'id': 3,
         'name': 'eyes',
         'directory': 'Expressions',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': None
     },
     {
         'id': 4,
         'name': 'head_gear',
         'directory': 'Head Gear',
         'required': False,
-        'rarity_weights': None,
+        'rarity_weights': None
     },
     {
         'id': 5,
         'name': 'clothes',
         'directory': 'Shirt',
         'required': False,
-        'rarity_weights': None,
+        'rarity_weights': None
     },
     {
         'id': 6,
@@ -75,6 +75,10 @@ CONFIG = [
         'name': 'wristband',
         'directory': 'Wristband',
         'required': False,
-        'rarity_weights': None,
+        # There should be 9 types of wristbands, but I could only find one on Github
+        # 'rarity_weights': [100, 5, 5, 15, 5, 5, 15, 15, 5, 1] 
+        # The first weight is because 'required' == False.
+        #       thus we have only 1 in 11 with a yellow wristband, the rest has no wristband.
+        'rarity_weights': [10, 1] 
     },
 ]
